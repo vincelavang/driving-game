67cd9daa5ab23c5d984abf138d2c400299bdfd26
Vince Lavang <vincelavang@gmail.com>
2017-06-30 14:00:15 -0700

    Added setInterval and draw functions. #2

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.querySelector('#canvas')
-  const context = canvas.getContext('2d')
-  let x = 0
-  let y = 0
-  const image = document.querySelector('#source')
-  image.addEventListener('load', () => {
-    context.drawImage(image, x, y, 100, 100)
-  })
+const canvas = document.querySelector('#canvas')
+const context = canvas.getContext('2d')
+let x = 0
+let y = 0
+let speed = 1
+let mod = 0
+setInterval(function () {
+  draw()
+}, 30)
+const image = document.querySelector('#source')
+
+image.addEventListener('load', () => {
+  context.drawImage(image, x, y, 100, 100)
 })
+
+function draw() {
+  x += speed * mod
+  y += speed * mod
+  context.save()
+  context.translate(x, y)
+  context.drawImage(image, x, y, 100, 100)
+  context.restore()
+}
