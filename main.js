8d371f4f3b6beb50fbaa4376941e7aeade6e7677
Vince Lavang <vincelavang@gmail.com>
2017-06-30 15:05:34 -0700

    Erased previous content car was leaving. #2

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const canvas = document.querySelector('#canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 const context = canvas.getContext('2d')
 let x = 0
 let y = 0
-let speed = 1
+let speed = 5
 let mod = 0
 setInterval(function () {
   draw()
@@ -14,8 +16,7 @@ image.addEventListener('load', () => {
 })
 
 function draw() {
-  x += speed * mod
-  y += speed * mod
+  context.clearRect(0, 0, canvas.width, canvas.height)
   context.save()
   context.translate(x, y)
   context.drawImage(image, x, y, 100, 100)
@@ -24,15 +25,19 @@ function draw() {
 
 document.addEventListener('keydown', function (event) {
   if (event.keyCode === 87) {
-    mod = 1
+    mod = -1
+    y += speed * mod
   }
   if (event.keyCode === 83) {
-    mod = -1
+    mod = 1
+    y += speed * mod
   }
   if (event.keyCode === 65) {
     mod = -1
+    x += speed * mod
   }
   if (event.keyCode === 68) {
     mod = 1
+    x += speed * mod
   }
 })
